c18b563880799ad0b3081ef72f34a9d2f3f34d71
WuYongbo <wuyongbo@WilburWu.local>
2016-06-27 01:37:43 +0800

    fix function error

---
+++ app/src/main/java/roff/startuparch/core/api/interceptor/TrafficMonitorInterceptor.java
@@ -0,0 +1,29 @@
+package roff.startuparch.core.api.interceptor;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by wuyongbo on 16-6-25.
+ */
+public class TrafficMonitorInterceptor implements Interceptor {
+
+    final String TAG = TrafficMonitorInterceptor.class.getSimpleName();
+
+    @Override
+    public Response intercept(Interceptor.Chain chain) throws IOException {
+
+        Request request = chain.request();
+
+        Response response = chain.proceed(request);
+
+        Log.d(TAG, "request content =" + response.toString());
+
+        return response;
+    }
+}
